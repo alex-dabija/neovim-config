6883b935ef26bc1431e8b2981a9f337fc9918ad7
Alex Dabija <alex.dabija@gmail.com>
2022-09-13 10:41:25 +0200

    build neovim with minimum required dependencies

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Alex's Neovim config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    neovim = {
+      url = "github:neovim/neovim?rev=e8ee6733926db83ef216497a1d660a173184ff39";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        callPackage = path: src: overrides:
+          let f = import path { inherit src; };
+          in f ((builtins.intersectAttrs (builtins.functionArgs f) pkgs) // overrides);
+
+      in {
+        packages = {
+          default = callPackage ./lib/my-neovim.nix inputs.neovim {
+          };
+        };
+      });
+}
---
+++ lib/my-neovim.nix
@@ -0,0 +1,34 @@
+{ src }:
+{ lib, stdenv, cmake, libuv, msgpack, luajit, tree-sitter, unibilium, libtermkey, libvterm-neovim, gperf, gettext }:
+let
+  luaEnv = luajit.withPackages (ps:
+    with ps; [ lpeg mpack ]
+  );
+in
+  stdenv.mkDerivation {
+    pname = "my-neovim";
+    version = "0.7.2";
+
+    inherit src;
+
+    patches = [
+      ./system_rplugin_manifest.patch
+    ];
+
+    buildInputs = [
+      libuv
+      luajit.pkgs.libluv
+      msgpack
+      tree-sitter
+      luaEnv
+      unibilium
+      libtermkey
+      libvterm-neovim
+      gperf
+    ];
+
+    nativeBuildInputs = [
+      cmake
+      gettext
+    ];
+  }
